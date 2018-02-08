7fde6e47f55c3e163354328eafcbbf95fb5466de
luludada <mingyangye801@gmail.com>
2018-02-08 02:06:22 -0800

    Add autofunctions

---
+++ Pros/include/autofunctions.h
@@ -0,0 +1,20 @@
+#ifndef AUTOFUNCTIONS_H_
+#define AUTOFUNCTIONS_H_
+
+#include "main.h"
+
+void stopEverything(); //function to stop everything
+
+void motorSpeed(int value, int speed); //Limit Speed
+
+void drive(int direction, int target); //Auto Drive
+
+void turn(int direction, int targetTurn, int timeout, float kp, float, kd); //Auto turn
+
+void mogoIn(int timeout);
+
+void mogoOut(int timeout);
+
+void mogoScore();
+
+void LineTrack();

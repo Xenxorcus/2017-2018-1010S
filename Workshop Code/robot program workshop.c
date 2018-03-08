c041f0dd86510837789a058271f6456388fcc11a
aim21320 <aim21320@gmail.com>
2018-03-07 17:59:38 -0800

    1011 code
    Drive code and set void

--- Workshop Code/robot program workshop.c
+++ Workshop Code/robot program workshop.c
@@ -192,7 +192,7 @@ task usercontrol()
 
 		}
 
-	// This is the main execution loop for the user control program.
+		// This is the main execution loop for the user control program.
 		// Each time through the loop your program should update motor + servo
 		// values based on feedback from the joysticks.
 
